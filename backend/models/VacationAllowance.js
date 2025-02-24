f707b4bcc1eb28930a7fe4bc99786046b5fca5b9
Ricabb <57535197+Ricabb@users.noreply.github.com>
2025-02-24 11:08:15 -0500

    Removed invalid Windows path

--- backend/models/VacationAllowance.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const VacationAllowanceSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  projectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
-  remainingHours: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('VacationAllowance', VacationAllowanceSchema);
